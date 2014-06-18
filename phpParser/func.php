f276d65f6e8391f4637e2d9b161cf47d16ab8a10
vohulg <vohulg@gmail.com>
2014-06-18 18:44:29 +0600

    18 june

--- phpParser/func.php
+++ phpParser/func.php
@@ -22,21 +22,34 @@ function get_id($str)
     
 }
 
-
-function getMainPage($url, $fileCookie)
+function getAuthJavaScript($url, $fileCookieIn)
 {
         $ch = curl_init(); 
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_HEADER, false);
 	curl_setopt($ch, CURLOPT_URL,$url); // set url to post to 
-	curl_setopt($ch, CURLOPT_FAILONERROR, 1); 
-	curl_setopt($ch, CURLOPT_VERBOSE, 1);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);// allow redirects 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); // return into a variable 
 	curl_setopt($ch, CURLOPT_TIMEOUT, 10); // times out after 4s 
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (Windows; U; Windows NT 5.0; En; rv:1.8.0.2) Gecko/20070306 Firefox/1.0.0.4");
-	curl_setopt($ch, CURLOPT_COOKIEJAR, $fileCookie);
-	curl_setopt($ch, CURLOPT_COOKIEFILE, $fileCookie);
+	curl_setopt($ch, CURLOPT_COOKIEJAR, $fileCookieIn);  //В какой файл записывать
+       // curl_setopt($ch, CURLOPT_VERBOSE, 1);
+	$result = curl_exec($ch); // run the whole process 
+	curl_close($ch); 
+	return $result;    
+    
+}
+
+
+function getMainPage($url, $fileCookieIn)
+{
+        $ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL,$url); // set url to post to 
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);// allow redirects 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); // return into a variable 
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10); // times out after 4s 
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (Windows; U; Windows NT 5.0; En; rv:1.8.0.2) Gecko/20070306 Firefox/1.0.0.4");
+	curl_setopt($ch, CURLOPT_COOKIEJAR, $fileCookieIn);  //В какой файл записывать
+	//curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        //curl_setopt($ch, CURLOPT_COOKIEFILE, $fileCookieOut); // из какого файла читать
 	$result = curl_exec($ch); // run the whole process 
 	curl_close($ch); 
 	return $result;    
@@ -46,16 +59,17 @@ function getMainPage($url, $fileCookie)
 
 function printDebug($param) 
 {
-  echo "********************\n";
+  echo "\n***********start*************************\n";
   echo $param;
-  echo "\n********************\n";
+  echo "\n************end*************************\n";
 }
 
-function getAuth($url, $idCreepy, $fileCookie)
+function getAuth($url, $idCreepy, $fileCookieIn, $fileCookieOut)
 {
         
-    $request = "email=vohulg@gmail.com&password=19791979&cookies=&rememberMe=1&id=".$idCreepy;
-    printDebug($request);
+    $request = "email=vohulg@gmail.com&password=19791979&cookies=&rememberMe=0&id=".$idCreepy;
+    
+     printDebug($request);
     
     $ch = curl_init(); 
         //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
@@ -69,8 +83,8 @@ function getAuth($url, $idCreepy, $fileCookie)
         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (Windows; U; Windows NT 5.0; En; rv:1.8.0.2) Gecko/20070306 Firefox/1.0.0.4");
 	curl_setopt($ch, CURLOPT_POST, 1); // set POST method 
         curl_setopt($ch, CURLOPT_POSTFIELDS, $request); // add POST fields
-	curl_setopt($ch, CURLOPT_COOKIEJAR, $fileCookie);
-	//curl_setopt($ch, CURLOPT_COOKIEFILE, $fileCookieOut);
+	curl_setopt($ch, CURLOPT_COOKIEJAR, $fileCookieIn);
+	curl_setopt($ch, CURLOPT_COOKIEFILE, $fileCookieOut);
 	$result = curl_exec($ch); // run the whole process 
         curl_close($ch);
     
@@ -99,19 +113,24 @@ function getProfile($url, $fileCookieOut)
     
 }
 
-function getTimeout($url, $fileCookieIn )
+function getTimeout($url, $fileCookieIn, $fileCookieOut )
 {
      $ch = curl_init(); 
-	curl_setopt($ch, CURLOPT_HEADER, false);
+	//Referer: http://kolesa.kz/passport/login
+        //curl_setopt($ch, CURLOPT_REFERER, true);
+        //curl_setopt($ch, CURLOPT_HEADER, true);
+       //curl_setopt($ch, CURLOPT_REFERER, "http://kolesa.kz/passport/login");
 	curl_setopt($ch, CURLOPT_URL,$url); // set url to post to 
-	curl_setopt($ch, CURLOPT_FAILONERROR, 1); 
+	//curl_setopt($ch, CURLOPT_FAILONERROR, 1); 
 	curl_setopt($ch, CURLOPT_VERBOSE, 1);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);// allow redirects 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); // return into a variable 
+       
+         //curl_setopt($ch, CURLOPT_ACCEPT_ENCODING, "gzip"); // return into a variable 
 	curl_setopt($ch, CURLOPT_TIMEOUT, 10); // times out after 4s 
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (Windows; U; Windows NT 5.0; En; rv:1.8.0.2) Gecko/20070306 Firefox/1.0.0.4");
 	curl_setopt($ch, CURLOPT_COOKIEJAR, $fileCookieIn);
-	//curl_setopt($ch, CURLOPT_COOKIEFILE, $fileCookieIn);
+	curl_setopt($ch, CURLOPT_COOKIEFILE, $fileCookieOut);
 	$result = curl_exec($ch); // run the whole process 
 	curl_close($ch); 
 	return $result;   
--- phpParser/main-parser.php
+++ phpParser/main-parser.php
@@ -1,41 +1,62 @@
 <?php
 include 'func.php';
 
-$fileCookie = "cookie.txt";
+//unlink("php_errors.log");
+unlink("cookieTimeout.txt");
+unlink("cookiePassportIn.txt");
+unlink("cookieCreepy.txt");
+
+$fileCookieIn = "cookiePassportIn.txt";
+$fileCookieOut = "cookiePassportOut.txt";
+
+// ---------первый запрос ---------------------------//
 $url = "http://kolesa.kz/passport/login";
-$str = getMainPage ($url, $fileCookie);
+$str = getMainPage ($url, $fileCookieIn );
+
+/*
+$text = file_get_contents($fileCookieIn);
+$array = explode("\n", $text);
+$newstr = '';
+for ($i=0; $i < 5; $i++)
+  $newstr .= $array[$i]."\n"; 
+file_put_contents($fileCookieIn, $newstr);
+ * */
+ 
+
+
+//--- отправляем второй запрос на http://id.creepycrux.com/auth.js?r=5312
 $url = get_creepRequestUrl($str);
 printDebug($url);
-
 $fileCookie = "cookieCreepy.txt";
-getMainPage ($url, $fileCookie);
+getAuthJavaScript ($url, $fileCookie);
 $str = file_get_contents($fileCookie);
 $idCreepy = get_id($str);
 
 printDebug("idCreepy:".$idCreepy);
 
-// -----post запрос на авторизацию--------------------//
+
+// -----третий post запрос на авторизацию--------------------//
 $url = "http://id.creepycrux.com/login.json";
 $fileCookieIn = "cookieAuthIn.txt";
-$fileCookieOut = "cookieAuthOut.txt";
-$res = getAuth ($url, $idCreepy, $fileCookieIn);
+$fileCookieOut = "cookieCreepy.txt";
+$res = getAuth ($url, $idCreepy, $fileCookieIn, $fileCookieOut);
 //------------------------------------------------//
 
-// ---- промежуточный запрос на получение kluid -----//
+// ---- четвертый промежуточный запрос на получение kluid -----//
+//http://kolesa.kz/my?sessId=jasjobuf5mfubfkqihcvpei3q6&sessTimeout=31536000
 $str = file_get_contents($fileCookie);
 $id = get_id($str);
 printDebug("idforTimeOut:".$id);
-
-//http://kolesa.kz/my?sessId=jasjobuf5mfubfkqihcvpei3q6&sessTimeout=31536000
-$url = "http://kolesa.kz/my?sessId=".$id."&sessTimeout=31536000";
+$url = "http://kolesa.kz/my?sessId=".$id."&sessTimeout=0";
 printDebug("urlTimeout:".$url);
 $fileCookieIn = "cookieTimeout.txt";
-getTimeout($url, $fileCookieIn );
+$fileCookieOut = "cookiePassportIn.txt";
+$res = getTimeout($url, $fileCookieIn, $fileCookieOut );
+//printDebug("getTimeout:".$res);
+
+
 
 return;
-
-
-
 
 // ----- запрос на получение web страницы профиля ------- //
 
